618537003d7dbd4bf71454a4dca3b1c894536e34
Kateryna Taranenko <ekaterina.a.taranenko@gmail.com>
2025-12-02 08:57:59 -0500

    Refactor CheckBSTValidity to track visited nodes

--- BSTChecker.h
+++ BSTChecker.h
@@ -1,69 +1,61 @@
 #ifndef BSTCHECKER_H
 #define BSTCHECKER_H
-
 // TODO: Include any needed header files
 #include "BSTNode.h"
-
-
 
 class BSTChecker {
 public:
    // TODO: Add any desired utility functions
-
-    static BSTNode* FindDuplicate(BSTNode* node, int key, BSTNode* skip) {
-        if (node == nullptr) {
-            return nullptr;
-        }
-
-        // Found duplicate (but not the same node we're checking)
-        if (node->key == key && node != skip) {
-            return node;
-        }
-
-        // Search left subtree
-        BSTNode* leftDup = FindDuplicate(node->left, key, skip);
-        if (leftDup != nullptr) {
-            return leftDup;
-        }
-
-        // Search right subtree
-        return FindDuplicate(node->right, key, skip);
-    }
-
-    static BSTNode* IsValid(BSTNode* node, int* min, int* max, BSTNode* root) {
-        if (node == nullptr) {
-            return nullptr;
-        }
-
-        // Check min constraint
-        if (min != nullptr && node->key <= *min) {
-            return node;
-        }
-
-        // Check max constraint
-        if (max != nullptr && node->key >= *max) {
-            return node;
-        }
-        // Check for duplicates in entire tree
-        if (FindDuplicate(root, node->key, node) != nullptr) {
-            return node;
-        }
-
-        // Check left subtree (preorder)
-        BSTNode* leftViolation = IsValid(node->left, min, &node->key, root);
-        if (leftViolation != nullptr) {
-            return leftViolation;
-        }
-
-        // Check right subtree
-        BSTNode* rightViolation = IsValid(node->right, &node->key, max, root);
-        if (rightViolation != nullptr) {
-            return rightViolation;
-        }
-
-        return nullptr;
-    }
-
+   
+   static bool HasBeenVisited(BSTNode** visited, int count, BSTNode* node) {
+      for (int i = 0; i < count; i++) {
+         if (visited[i] == node) {
+            return true;
+         }
+      }
+      return false;
+   }
+   
+   static BSTNode* IsValid(BSTNode* node, int* min, int* max, 
+                           BSTNode** visited, int* visitCount) {
+      if (node == nullptr) {
+         return nullptr;
+      }
+      
+      // Check if this node has been visited before (catches structural problems)
+      if (HasBeenVisited(visited, *visitCount, node)) {
+         return node;
+      }
+      
+      // Mark node as visited
+      visited[*visitCount] = node;
+      (*visitCount)++;
+      
+      // Check min constraint (node must be greater than min)
+      if (min != nullptr && node->key <= *min) {
+         return node;
+      }
+      
+      // Check max constraint (node must be less than max)
+      if (max != nullptr && node->key >= *max) {
+         return node;
+      }
+      
+      // Preorder traversal: check left subtree first
+      BSTNode* leftViolation = IsValid(node->left, min, &node->key, visited, visitCount);
+      if (leftViolation != nullptr) {
+         return leftViolation;
+      }
+      
+      // Then check right subtree
+      BSTNode* rightViolation = IsValid(node->right, &node->key, max, visited, visitCount);
+      if (rightViolation != nullptr) {
+         return rightViolation;
+      }
+      
+      return nullptr;
+   }
+   
    // CheckBSTValidty() determines if the tree is a valid BST. If so, nullptr
    // is returned. If not, the first (in preorder traversal) node in violation
    // of BST requirements is returned. Such a node will be one of the following:
@@ -71,17 +63,15 @@ public:
    // - A node in the right subtree of an ancestor with a greater or equal key
    // - A node that is encountered more than once during traversal
    static BSTNode* CheckBSTValidity(BSTNode* rootNode) {
-      // TODO: Type your code here (remove placeholer line below)
-
-       //Base case:
-       if (rootNode == nullptr)
-       {
-           return nullptr;
-       }
-       
-       return IsValid(rootNode, nullptr, nullptr, rootNode);
-
+      if (rootNode == nullptr) {
+         return nullptr;
+      }
+      
+      // Array to track visited nodes (assuming max 100000 nodes)
+      BSTNode* visited[100000];
+      int visitCount = 0;
+      
+      return IsValid(rootNode, nullptr, nullptr, visited, &visitCount);
    }
 };
-
 #endif
